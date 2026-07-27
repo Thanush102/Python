74348b5dd626242822e09a11281eb2f7cda857bc
Thanush102 <thanushveeramosu@gmail.com>
2026-07-27 12:44:19 +0530

    Delete identifiers.py

--- identifiers.py
+++
@@ -1,9 +0,0 @@
-student_name="thanush"
-STUDENT_NUMBER="25341A05P7"
-student_acdemic_year="2025-2029"
-subject_name="problem solving using python"
-def student_semester():
-    print("3rd semester");
-print(student_name,STUDENT_NUMBER,student_acdemic_year,subject_name)
-student_semester()
-    
