25be01e947c367395ae5138c8ba86a7cc592776f
Thanush102 <thanushveeramosu@gmail.com>
2026-09-26 12:01:59 +0530

    Add files via upload

---
+++ Lab1_Task1.py
@@ -0,0 +1,15 @@
+def greet(name):
+    print(f"Hello,{name}! welcome to python")
+name1='Thanush'
+name2='Nani'
+name3='Kushal'
+greet(name1)
+greet(name2)
+greet(name3)
+
+'''
+output:
+Hello,Thanush! welcome to python
+Hello,Nani! welcome to python
+Hello,Kushal! welcome to python
+'''
